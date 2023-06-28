47d9db4527c64f414cab2040f8bdfb46d27a449a
AndreaRomano02 <andy.romano2002@gmail.com>
2023-06-28 14:27:20 +0200

    Add hello world

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>LA MIA PIRMA APP CON VITE!!!!!</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+  text-align: center;
+  margin-top: 200px;
+  padding: 50px;
+  background-color: gray;
+}
+</style>
